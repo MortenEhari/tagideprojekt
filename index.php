f328a3879774c8778343e112d0c8989cc6adadc0
FalseDeal <drfalsedeal@gmail.com>
2015-09-09 13:39:21 +0300

    Test commit ja html skeleton

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- meta tags -->
+        <meta name="author" content="mor">
+        <meta name="keywords" content="">
+        <meta name="description" content="">
+        <meta charset="UTF-8">
+
+        <!-- title & favicon -->
+        <title>
+            Tagide projekt
+        </title>
+        <link rel="icon"
+              type="image/ico"
+              href="favicon.ico">
+
+        <!-- external css file -->
+        <link rel="stylesheet" type="text/css" href="styles.css">
+    </head>
+    <body>
+        <div id="header">
+
+        </div>
+        <div id="container">
+            <div id="navigation">
+
+            </div>
+            <div id="content">
+
+            </div>
+        </div>
+        <div id="footer">
+
+        </div>
+    </body>
+</html>
